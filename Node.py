d0a2f61cdeca919af68b8a466428c375e98b7186
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-08-18 19:01:08 -0300

    Teste de Distribuição

--- Node.py
+++ Node.py
@@ -7,10 +7,10 @@ def run(rank, size):
     if rank == 0:
         tensor += 1
         dist.send(tensor=tensor, dst=1)
-        print(f'Rank {rank} sent data {tensor[0]}')
+        print(f'Rank {rank} enviou dados {tensor[0]}')
     elif rank == 1:
         dist.recv(tensor=tensor, src=0)
-        print(f'Rank {rank} received data {tensor[0]}')
+        print(f'Rank {rank} recebeu dados {tensor[0]}')
 
 def init_processes(rank, size, fn, master_ip, backend='gloo'):
     dist.init_process_group(backend, init_method=f'tcp://{master_ip}:29500', rank=rank, world_size=size)
@@ -18,5 +18,6 @@ def init_processes(rank, size, fn, master_ip, backend='gloo'):
 
 if __name__ == "__main__":
     size = 2
-    master_ip = "localhost"
-    init_processes(0, size, run, master_ip)
+    master_ip = input("Digite o IP do nó mestre: ")
+    rank = int(input("Digite o rank deste nó (0 para mestre, 1 para trabalhador): "))
+    init_processes(rank, size, run, master_ip)
