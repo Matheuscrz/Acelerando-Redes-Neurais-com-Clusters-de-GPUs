14a8c83bf4e83e9c44dc10d4d1c1504c77b6ce30
Matheuscrz <matheuslimasof.eng@gmail.com>
2024-08-18 17:45:48 -0300

    Teste de distribuição com pytorch. Codigo do nó master.

---
+++ master_node.py
@@ -0,0 +1,22 @@
+import torch
+import torch.distributed as dist
+from torch.multiprocessing import Process
+
+def run(rank, size):
+    tensor = torch.zeros(1)
+    if rank == 0:
+        tensor += 1
+        dist.send(tensor=tensor, dst=1)
+        print(f'Rank {rank} sent data {tensor[0]}')
+    elif rank == 1:
+        dist.recv(tensor=tensor, src=0)
+        print(f'Rank {rank} received data {tensor[0]}')
+
+def init_processes(rank, size, fn, master_ip, backend='gloo'):
+    dist.init_process_group(backend, init_method=f'tcp://{master_ip}:29500', rank=rank, world_size=size)
+    fn(rank, size)
+
+if __name__ == "__main__":
+    size = 2
+    master_ip = "localhost"
+    init_processes(0, size, run, master_ip)
